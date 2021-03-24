2f2d576962b2ed2fbfb0cbe314650497ecb6342d
Alyssia <1933115@etu.cegepjonquiere.ca>
2021-03-24 10:57:27 -0400

    Please work

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,10 +1,28 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 public class GameFrame extends JFrame{
 
+    public Rectangle niveau1 = new Rectangle(GamePanel.LARGEUR_ECRAN/2 - 75, 100, 150, 50 );
+    private JButton play = new JButton("Play");
     GameFrame(){
+
+      /*  if(GamePanel.state == GamePanel.STATE.MENU) {
+            play.setBounds(niveau1);
+            this.add(play);
+            play.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent actionEvent) {
+                    GamePanel.state = GamePanel.STATE.GAME;
+                    GamePanel.timer.start();
+                }
+            });
+        }
+        */
 
         this.add(new GamePanel());
         this.setTitle("Chats");
@@ -16,7 +34,7 @@ public class GameFrame extends JFrame{
 
     }
 
-    public class MyKeyAdapter extends KeyAdapter {
+   /* public class MyKeyAdapter extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e) {
             switch (e.getKeyCode()) {
@@ -27,7 +45,7 @@ public class GameFrame extends JFrame{
                         GamePanel.state = GamePanel.STATE.MENU;
             }
         }
-    }
+    }*/
     public void choisirPanel() {
 
     }
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -21,6 +21,8 @@ public class GamePanel extends JPanel implements ActionListener{
     int positionY;
     int playerX = GROSSEUR_UNITE;
     int playerY = GROSSEUR_UNITE;
+    int porteX = LARGEUR_ECRAN - GROSSEUR_UNITE;
+    int porteY = LARGEUR_ECRAN - GROSSEUR_UNITE;
     char direction = 'R';
     int[][] mur = new int[LARGEUR_ECRAN][HAUTEUR_ECRAN];
     boolean mursL = false;
@@ -31,17 +33,26 @@ public class GamePanel extends JPanel implements ActionListener{
         GAME,
         MENU
     }
-    public static STATE state = STATE.GAME;
+    public static enum NIVEAU{
+        M,
+        N1,
+        N2,
+        N3,
+        N4,
+        N5,
+        N6,
+        N7
+    }
+    public static STATE state = STATE.MENU;
+    public static NIVEAU niveau = NIVEAU.M;
+
     MainMenu menu;
 
     static final int DELAY = 50;
     Random random;
     static boolean gameOn = false;
-    Timer timer;
+    public static Timer timer;
 
-    public Rectangle niveau1 = new Rectangle(GamePanel.LARGEUR_ECRAN/2 - 75, 100, 150, 50 );
-    public Rectangle niveau2 = new Rectangle(GamePanel.LARGEUR_ECRAN/2 - 75, 200, 150, 50 );
-    public Rectangle niveau3 = new Rectangle(GamePanel.LARGEUR_ECRAN/2 - 75, 300, 150, 50 );
 
     Cursor cursor = new Cursor(Cursor.HAND_CURSOR);
 
@@ -66,26 +77,53 @@ public class GamePanel extends JPanel implements ActionListener{
     }
 
     public void start() {
-
         ouvert = true;
-        if(state == STATE.GAME) {
-            nouvelleProie();
-            timer = new Timer(DELAY, this);
-            timer.start();
-        }
+        nouvelleProie();
+        timer = new Timer(DELAY, this);
+    }
 
+    public void menu(){
+        playerX = GROSSEUR_UNITE;
+        playerY = GROSSEUR_UNITE;
+        state = STATE.MENU;
+        niveau = NIVEAU.M;
+        timer.stop();
+    }
+
+    public void niveau1(){
+        state = STATE.GAME;
+        niveau = NIVEAU.N1;
+        timer.start();
+    }
+
+    public void niveau2(){
+        state = STATE.GAME;
+        niveau = NIVEAU.N2;
+        timer.start();
+    }
+
+    public void niveau3(){
+        state = STATE.GAME;
+        niveau = NIVEAU.N3;
+        timer.start();
+    }
+
+    public void finNiveau(){
+        if (playerX == porteX && playerY == porteY) {
+            menu();
+        }
     }
 
     public void pause() {
         label.setFont(new Font("Ink Free", Font.BOLD, 40));
         label.setVisible(true);
-        GamePanel.gameOn = true;
+        gameOn = true;
         timer.stop();
     }
 
     public void resume() {
         label.setVisible(false);
-        GamePanel.gameOn = false;
+        gameOn = false;
         timer.start();
     }
 
@@ -95,42 +133,47 @@ public class GamePanel extends JPanel implements ActionListener{
     }
 
     public void dessiner(Graphics g) {
+
         if(state == STATE.GAME) {
             if (ouvert) {
                 g.setColor(new Color(100, 166, 189));
                 g.fillRect(playerX, playerY, GROSSEUR_UNITE, GROSSEUR_UNITE);
             }
             for (int i = 0; i < (HAUTEUR_ECRAN / GROSSEUR_UNITE); i++) {
-                    g.setColor(new Color(173, 167, 201));
-                    g.drawLine(i * GROSSEUR_UNITE, 0, i * GROSSEUR_UNITE, HAUTEUR_ECRAN);
-                    g.drawLine(0, i * GROSSEUR_UNITE, LARGEUR_ECRAN, i * GROSSEUR_UNITE);
-                }
+                g.setColor(new Color(173, 167, 201));
+                g.drawLine(i * GROSSEUR_UNITE, 0, i * GROSSEUR_UNITE, HAUTEUR_ECRAN);
+                g.drawLine(0, i * GROSSEUR_UNITE, LARGEUR_ECRAN, i * GROSSEUR_UNITE);
+            }
 
-                g.setColor(Color.BLUE);
-                g.fillOval(positionX, positionY, GROSSEUR_UNITE, GROSSEUR_UNITE);
 
-                for(int i = 0; i < GROSSEUR_UNITE * 8; i++){
+
+            if (niveau == NIVEAU.N1) {
+                for (int i = 0; i < GROSSEUR_UNITE * 8; i++) {
                     mur[GROSSEUR_UNITE * 3][i] = 1;
                     mur[GROSSEUR_UNITE * 4][i] = 1;
                     g.setColor(Color.GRAY);
                     g.fillRect(GROSSEUR_UNITE * 3, i, GROSSEUR_UNITE, GROSSEUR_UNITE);
                     g.fillRect(GROSSEUR_UNITE * 4, i, GROSSEUR_UNITE, GROSSEUR_UNITE);
                 }
-                for(int i = GROSSEUR_UNITE * 6; i < LARGEUR_ECRAN; i++){
+                for (int i = GROSSEUR_UNITE * 6; i < LARGEUR_ECRAN; i++) {
                     mur[i][GROSSEUR_UNITE * 14] = 1;
                     mur[i][GROSSEUR_UNITE * 15] = 1;
                     g.setColor(Color.GRAY);
                     g.fillRect(i, GROSSEUR_UNITE * 14, GROSSEUR_UNITE, GROSSEUR_UNITE);
                     g.fillRect(i, GROSSEUR_UNITE * 15, GROSSEUR_UNITE, GROSSEUR_UNITE);
                 }
+                porteX = LARGEUR_ECRAN - (GROSSEUR_UNITE * 2);
+                porteY = GROSSEUR_UNITE;
+                g.setColor(Color.RED);
+                g.fillRect(porteX, porteY, GROSSEUR_UNITE, GROSSEUR_UNITE);
 
+                g.setColor(Color.BLUE);
+                g.fillOval(positionX, positionY, GROSSEUR_UNITE, GROSSEUR_UNITE);
+            }
 
         }
-        if(state == STATE.MENU) {
-            g.setColor(Color.RED);
-            g.setFont(new Font("Ink Free", Font.BOLD, 40));
-            FontMetrics metrics = g.getFontMetrics(g.getFont());
-            g.drawString("Une histoire de reflexion", (GamePanel.LARGEUR_ECRAN - metrics.stringWidth("Une histoire de reflexion")) / 2, g.getFont().getSize());
+        else if (state == STATE.MENU) {
+           menu.rendu(g);
         }
 
     }
@@ -142,22 +185,24 @@ public class GamePanel extends JPanel implements ActionListener{
 
     public void verifierCollision() {
 
-        for(int z = 0; z <= GROSSEUR_UNITE * 9; z++) {
-            if (playerX == GROSSEUR_UNITE * 5 && playerY == z - 1) {
-                mursL = true;
-            } else if (playerX == GROSSEUR_UNITE * 2 && playerY == z - 1) {
-                mursR = true;
-            } else if ((playerX == GROSSEUR_UNITE * 3 || playerX == GROSSEUR_UNITE * 4) && playerY == z + 1) {
-                mursU = true;
+        if(niveau == NIVEAU.N1) {
+            for (int z = 0; z <= GROSSEUR_UNITE * 9; z++) {
+                if (playerX == GROSSEUR_UNITE * 5 && playerY == z - 1) {
+                    mursL = true;
+                } else if (playerX == GROSSEUR_UNITE * 2 && playerY == z - 1) {
+                    mursR = true;
+                } else if ((playerX == GROSSEUR_UNITE * 3 || playerX == GROSSEUR_UNITE * 4) && playerY == z + 1) {
+                    mursU = true;
+                }
             }
-        }
-        for(int i = GROSSEUR_UNITE * 5; i < LARGEUR_ECRAN; i++) {
-            if (playerY == GROSSEUR_UNITE * 13 && playerX == i - 1) {
-                mursD = true;
-            } else if (playerY == GROSSEUR_UNITE * 16 && playerX == i - 1) {
-                mursU = true;
-            } else if ((playerY == GROSSEUR_UNITE * 14 || playerY == GROSSEUR_UNITE * 15 )&& playerX == i - 1) {
-                mursR = true;
+            for (int i = GROSSEUR_UNITE * 5; i < LARGEUR_ECRAN; i++) {
+                if (playerY == GROSSEUR_UNITE * 13 && playerX == i - 1) {
+                    mursD = true;
+                } else if (playerY == GROSSEUR_UNITE * 16 && playerX == i - 1) {
+                    mursU = true;
+                } else if ((playerY == GROSSEUR_UNITE * 14 || playerY == GROSSEUR_UNITE * 15) && playerX == i - 1) {
+                    mursR = true;
+                }
             }
         }
     }
@@ -187,6 +232,7 @@ public class GamePanel extends JPanel implements ActionListener{
     public void actionPerformed(ActionEvent e) {
         if (ouvert) {
             verifierCollision();
+            finNiveau();
         }
         repaint();
     }
@@ -194,30 +240,10 @@ public class GamePanel extends JPanel implements ActionListener{
     public class MyKeyAdapter extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e) {
-          /*  switch (e.getKeyCode()){
-                case KeyEvent.VK_LEFT :
-                    direction = 'L';
-                    break;
-                case KeyEvent.VK_RIGHT :
-                    direction = 'R';
-                    break;
-                case KeyEvent.VK_UP :
-                    direction = 'U';
-                    break;
-                case KeyEvent.VK_DOWN :
-                    direction = 'D';
-                    break;
-                case KeyEvent.VK_ENTER:
-                    if(state == STATE.GAME){
-                        state = STATE.MENU;
-                    }
-                    else if(state == STATE.MENU)
-                        state = STATE.GAME;
-            } */
 
             switch(e.getKeyCode()){
                 case KeyEvent.VK_LEFT:
-                    if (playerX > 0 && timer.isRunning()) {
+                    if (playerX > 0 && timer.isRunning() && state == STATE.GAME) {
                         if(!mursL) {
                             playerX = playerX - GROSSEUR_UNITE;
                         }
@@ -226,7 +252,7 @@ public class GamePanel extends JPanel implements ActionListener{
                     }
                     break;
                 case KeyEvent.VK_RIGHT:
-                    if (playerX <= BORDER_NUMBER_X && timer.isRunning()) {
+                    if (playerX <= BORDER_NUMBER_X && timer.isRunning() && state == STATE.GAME) {
                         if(!mursR)
                             playerX = playerX + GROSSEUR_UNITE;
                         else
@@ -234,7 +260,7 @@ public class GamePanel extends JPanel implements ActionListener{
                     }
                     break;
                 case KeyEvent.VK_UP:
-                    if (playerY > 0 && timer.isRunning()) {
+                    if (playerY > 0 && timer.isRunning() && state == STATE.GAME) {
                         if(!mursU)
                             playerY = playerY - GROSSEUR_UNITE;
                         else
@@ -242,7 +268,7 @@ public class GamePanel extends JPanel implements ActionListener{
                     }
                     break;
                 case KeyEvent.VK_DOWN:
-                    if (playerY <= BORDER_NUMBER_Y && timer.isRunning()) {
+                    if (playerY <= BORDER_NUMBER_Y && timer.isRunning() && state == STATE.GAME) {
                         if(!mursD)
                             playerY = playerY + GROSSEUR_UNITE;
                         else
@@ -257,11 +283,14 @@ public class GamePanel extends JPanel implements ActionListener{
                         pause();
                     }
                     break;
-                case KeyEvent.VK_ENTER :
-                    if(state == STATE.MENU)
-                        state = STATE.GAME;
-                    else if (state == STATE.GAME)
-                        state = STATE.MENU;
+                case KeyEvent.VK_1 :
+                    if(state == STATE.MENU) {
+                        niveau1();
+                    }
+                case KeyEvent.VK_2 :
+                    if(state == STATE.MENU) {
+                        niveau2();
+                    }
                     break;
             }
         }
