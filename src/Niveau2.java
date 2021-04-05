4f2478700f7ac012b46240ee7e8d09b5000247e1
Alyssia <1933115@etu.cegepjonquiere.ca>
2021-04-05 18:29:55 -0400

    Please work

---
+++ src/Niveau2.java
@@ -0,0 +1,48 @@
+import java.awt.*;
+
+public class Niveau2 {
+
+    private int GROSSEUR_UNITE;
+    private int LARGEUR_ECRAN;
+
+    public Niveau2() {
+        GROSSEUR_UNITE = GamePanel.GROSSEUR_UNITE;
+        LARGEUR_ECRAN = GamePanel.LARGEUR_ECRAN;
+    }
+
+    public void rendu(Graphics g){
+        for (int i = GROSSEUR_UNITE; i < GROSSEUR_UNITE * 9; i++) {
+            g.setColor(Color.GRAY);
+            g.fillRect(i, GROSSEUR_UNITE, GROSSEUR_UNITE, GROSSEUR_UNITE);
+        }
+        for (int i = GROSSEUR_UNITE * 13; i < LARGEUR_ECRAN; i++) {
+            g.setColor(Color.GRAY);
+            g.fillRect(i, GROSSEUR_UNITE, GROSSEUR_UNITE, GROSSEUR_UNITE);
+        }
+        for (int i = GROSSEUR_UNITE * 3; i < GROSSEUR_UNITE * 7; i++) {
+            g.setColor(Color.GRAY);
+            g.fillRect(i, GROSSEUR_UNITE * 3, GROSSEUR_UNITE, GROSSEUR_UNITE);
+        }
+        for (int i = GROSSEUR_UNITE * 9; i < GROSSEUR_UNITE * 13; i++) {
+            g.setColor(Color.GRAY);
+            g.fillRect(i, GROSSEUR_UNITE * 3, GROSSEUR_UNITE, GROSSEUR_UNITE);
+        }
+        for (int i = LARGEUR_ECRAN; i > (LARGEUR_ECRAN - GROSSEUR_UNITE * 3); i--) {
+            g.setColor(Color.GRAY);
+            g.fillRect(i, GROSSEUR_UNITE * 3, GROSSEUR_UNITE, GROSSEUR_UNITE);
+        }
+        for (int i = GROSSEUR_UNITE * 3; i < GROSSEUR_UNITE * 7; i++) {
+            g.setColor(Color.GRAY);
+            g.fillRect(i, GROSSEUR_UNITE * 3, GROSSEUR_UNITE, GROSSEUR_UNITE);
+        }
+
+
+        GamePanel.porteX = LARGEUR_ECRAN - (GROSSEUR_UNITE * 2);
+        GamePanel.porteY = GROSSEUR_UNITE;
+        g.setColor(Color.RED);
+        g.fillRect(GamePanel.porteX, GamePanel.porteY, GROSSEUR_UNITE, GROSSEUR_UNITE);
+
+        g.setColor(Color.BLUE);
+        g.fillOval(GamePanel.positionX, GamePanel.positionY, GROSSEUR_UNITE, GROSSEUR_UNITE);
+    }
+}
